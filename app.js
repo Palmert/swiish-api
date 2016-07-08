7eaa3f432dea523eac01357a1a91cdf9a89cb8e3
palmert <thomapalmer@gmail.com>
2016-07-08 11:58:44 -0400

    Initial project setup. Using supervisor to monitor project changes and sequelize as an ORM connected to our postgres db.

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500).send(err.message);
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).send(err.message);
+});
+
+
+module.exports = app;
