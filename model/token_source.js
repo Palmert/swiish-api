57139cce824849db09f7fb54a621306bc3f8d0df
palmert <thomapalmer@gmail.com>
2016-07-09 17:54:15 -0400

    Create token_sources table and TokenSource model.

---
+++ model/token_source.js
@@ -0,0 +1,16 @@
+var sequelize = require('../database').sequelize;
+var db = require('../database').db;
+
+var TokenSource = db.define('token_sources', {
+  id: {
+    type: sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  source: {
+    type: sequelize.STRING,
+    allowNull: false,
+  }
+});
+
+module.exports = TokenSource;
