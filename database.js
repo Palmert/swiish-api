4e8751990e243524d435d557d89340279f5a92d1
palmert <thomapalmer@gmail.com>
2016-07-09 12:25:02 -0400

    Expose global database connection and test connection prior to starting server.

---
+++ database.js
@@ -0,0 +1,28 @@
+var Sequelize = require('sequelize');
+
+module.exports = {
+	sequelize: Sequelize,
+	db: new Sequelize('swiish_development', 'swiishuser', 'password', {
+    host: 'localhost',
+    dialect: 'postgres',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  }),
+
+	successCallback : function (data, cb) {
+		cb({
+			success: true,
+			data: data
+		});
+	},
+
+	errorCallback : function (error, cb) {
+		cb({
+			success: false,
+			error: error
+		});
+	}
+};
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var Sequelize = require('sequelize');
-const sequelize = new Sequelize('swiish_development', 'swiishuser', 'Rufus123', {
-  host: 'localhost',
-  dialect: 'postgres',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+
 
 router.get('/', function(req, res, next) {
-  sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(function (err) {
-    console.log('Unable to connect to the database:', err);
-  });
 });
 
 module.exports = router;
