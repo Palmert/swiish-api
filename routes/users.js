4dbb7fbab20c3a3efa5abebecefad15d587e4662
palmert <thom.palmer@trov.com>
2017-04-20 19:46:47 -0400

    Create login route

--- routes/users.js
+++ routes/users.js
@@ -2,26 +2,40 @@ var express = require('express');
 var router = express.Router();
 var db = require('../database').db;
 var User = require('../model/user');
+var bcrypt = require('bcryptjs');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
 })
 
-router.post('/', function(req, res) {
-  console.log(req.body);
-  return db.transaction(function (t) {
-    return User.create({
-        username: req.body.username,
-        virtual_password: req.body.virtual_password
-      });
-  }).then(function (result) {
-    console.log(result);
-    res.send('success');
-  }).catch(function (err) {
-    console.log(err);
-    res.status(500).send(err.name);
-  });
+router.post('/', function (req, res) {
+    console.log(req.body);
+    return db.transaction(function (t) {
+        return User.create({
+            username: req.body.username,
+            virtual_password: req.body.virtual_password
+        });
+    }).then(function (result) {
+        console.log(result);
+        res.send('success');
+    }).catch(function (err) {
+        console.log(err);
+        res.status(500).send(err.name);
+    });
+})
+
+router.post('/login', function (req, res) {
+    User.findById(req.body.username).then(function (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+            res.send("Login Success!");
+        } else {
+            res.status(500).send("Invalid username/password");
+        }
+    }, function (err) {
+        res.status(500).send(err.name);
+    });
 })
 
 module.exports = router;
